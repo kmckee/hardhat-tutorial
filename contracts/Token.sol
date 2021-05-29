f9662c1a3487172fa79b6d5c13b04420d6a8d1b8
Kyle McKee <mckee.kyle@gmail.com>
2021-05-29 08:05:33 -0400

    Added hardhat console logs

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.7.0;
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -35,6 +36,8 @@ contract Token {
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        console.log("Trying to send %s tokens to %s", amount, to);
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
