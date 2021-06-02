c423ecded0e0480d11a1e9eaff8e119aaee2307e
Kyle McKee <mckee.kyle@gmail.com>
2021-06-01 20:12:55 -0400

    npm run develop was deploying to hardhat not localhost.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,15 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.7.3",
   networks: {
+    hardhat: {
+      accounts: [
+        {
+          balance: "10000000000000000000",
+          privateKey:
+            "0xe8296150a5dfb0eaa1f0aa4fa9f09c43c8b5aee87ad2180d5f54b411da484318",
+        },
+      ],
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL,
       accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
